cdc8a2d145afb455f458b89037a409719da2d91e
kimtendu <kimtendu@gmail.com>
2018-05-28 16:22:34 +0300

    Add files via upload

---
+++ model_portfolio.php
@@ -0,0 +1,64 @@
+<?php
+
+class Model_Portfolio extends Model
+{
+	
+	public function get_data()
+	{	
+		
+		// Здесь мы просто сэмулируем реальные данные.
+		
+		return array(
+			
+			array(
+				'Year' => '2012',
+				'Site' => 'http://DunkelBeer.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2012',
+				'Site' => 'http://ZopoMobile.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2012',
+				'Site' => 'http://GeekWear.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2011',
+				'Site' => 'http://РоналВарвар.рф',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2011',
+				'Site' => 'http://TompsonTatoo.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2011',
+				'Site' => 'http://DaftState.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2011',
+				'Site' => 'http://TiltPeople.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+			array(
+				'Year' => '2011',
+				'Site' => 'http://AbsurdGames.ru',
+				'Description' => 'Bla bla bal".'
+			),
+
+		);
+	}
+
+}
