fd2e3f7e2ba0ba6493bedd2fcf720c514644c385
kimtendu <kimtendu@gmail.com>
2018-05-28 16:23:53 +0300

    Add files via upload

---
+++ views/contacts_view.php
@@ -0,0 +1,10 @@
+<h1>PLs do NOT contact </h1>
+<p>
+<ul class="list">
+						<li class="first">tel: 054-619-xx-xx</li>
+						<li>skypeid: kimtendu.is</li>
+						<li class="last">email: kimtendu@gmail.com</li>
+					</ul>
+<br/>
+
+</p>
